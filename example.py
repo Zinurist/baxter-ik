493034c055f11404bf84187b6f6a92e824499bcc
Nicolas Lindae <nicolas@lindae.de>
2019-08-31 17:10:54 +0200

    Improve pointing loss

--- example.py
+++ example.py
@@ -5,11 +5,14 @@ from baxter_ik import *
 # he should be roughly pointing at the goal position with his right gripper.
 
 object_position = np.array([1.294, -0.063, 0.003])
-joints = baxter_ik(create_pointing_loss(object_position, limb='right'))
-joints_dict = convert_joints_to_dict(joints, 'right')
-print('To point at object at position %s, move joints to positions %s.' % (object_position, joints_dict))
+res = baxter_ik(create_pointing_loss(object_position, limb='right', fix_joints=np.array([False, False, True, False, True, False, False])))
+print(res)
+joints_dict = convert_joints_to_dict(res.x, 'right')
+print('To point at object at position %s, move joints to positions %s. Loss: %s' % (object_position, joints_dict, res.fun))
 
 
-joints = baxter_ik(create_position_loss(object_position, limb='right'))
-joints_dict = convert_joints_to_dict(joints, 'right')
-print('To get to position %s as close as possible, move joints to positions %s.' % (object_position, joints_dict))
+res = baxter_ik(create_position_loss(object_position, limb='right'))
+print(res)
+joints_dict = convert_joints_to_dict(res.x, 'right')
+print('To get to position %s as close as possible, move joints to positions %s. Loss: %s' % (object_position, joints_dict, res.fun))
+    
